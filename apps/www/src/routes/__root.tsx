3b9a9393f5c373c54a32d9c1bee75f1e4dff4c0c
Athicha Leksansern <nongtonkaew@gmail.com>
2026-03-03 18:38:02 +0700

    Copy styling from v0 version

--- apps/www/src/routes/__root.tsx
+++ apps/www/src/routes/__root.tsx
@@ -23,6 +23,16 @@ export const Route = createRootRoute({
                 rel: 'stylesheet',
                 href: appCss,
             },
+            { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com',
+                crossOrigin: 'anonymous',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap',
+            },
         ],
     }),
 
