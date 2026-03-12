91fef80bd962f8a357f6eef699adc8c335abf267
Athicha Leksansern <nongtonkaew@gmail.com>
2026-03-12 17:24:06 +0700

    Add duffel searching hotels example

--- apps/www/src/routes/design-system/-components/duffel-api-section/actions.ts
+++ apps/www/src/routes/design-system/-components/duffel-api-section/actions.ts
@@ -8,13 +8,33 @@ export const searchFlights = createServerFn({ method: 'GET' }).handler(
                 {
                     origin: 'BKK',
                     destination: 'HND',
-                    departure_date: '2026-03-12',
+                    departure_date: '2026-04-12',
                     arrival_time: null,
                     departure_time: null,
                 },
             ],
             passengers: [{ type: 'adult' }, { type: 'adult' }, { age: 1 }],
             // cabin_class: 'economy',
+        })
+
+        return JSON.stringify(results)
+    },
+)
+
+export const searchHotels = createServerFn({ method: 'GET' }).handler(
+    async () => {
+        const results = await duffel.stays.search({
+            rooms: 1,
+            location: {
+                radius: 2,
+                geographic_coordinates: {
+                    longitude: -0.1416,
+                    latitude: 51.5071,
+                },
+            },
+            check_out_date: '2026-04-16',
+            check_in_date: '2026-04-12',
+            guests: [{ type: 'adult' }, { type: 'adult' }],
         })
 
         return JSON.stringify(results)
--- apps/www/src/routes/design-system/-components/duffel-api-section/index.tsx
+++ apps/www/src/routes/design-system/-components/duffel-api-section/index.tsx
@@ -1,32 +1,56 @@
 import { useQuery } from '@tanstack/react-query'
 import { useState } from 'react'
 import { IconLoader } from '@tabler/icons-react'
-import { searchFlights } from './actions'
+import { searchFlights, searchHotels } from './actions'
 import { Card } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
 
 export default function DuffelApiSection() {
-    const [enabled, isEnabled] = useState<boolean>(false)
-    const { data, isLoading } = useQuery({
+    const [enabledFlights, isEnabledFlights] = useState<boolean>(false)
+    const [enabledHotels, isEnabledHotels] = useState<boolean>(false)
+
+    const { data: flights, isLoading: isLoadingFlight } = useQuery({
         queryKey: ['duffel-flight-search'],
         queryFn: async () => {
             return await searchFlights()
         },
-        enabled,
+        enabled: enabledFlights,
+    })
+
+    const { data: hotels, isLoading: isLoadingHotel } = useQuery({
+        queryKey: ['duffel-hotel-search'],
+        queryFn: async () => {
+            return await searchHotels()
+        },
+        enabled: enabledHotels,
     })
 
     return (
-        <Card className="bg-background gap-4 p-4">
-            <Button onClick={() => isEnabled((prev) => !prev)}>
-                {enabled ? 'Disable' : 'Enable'} Flight Search
+        <Card className="bg-background grid grid-cols-2 gap-4 p-4">
+            <Button onClick={() => isEnabledFlights((prev) => !prev)}>
+                {enabledFlights ? 'Disable' : 'Enable'} Flight Search
             </Button>
 
-            {isLoading ? (
+            <Button onClick={() => isEnabledHotels((prev) => !prev)}>
+                {enabledHotels ? 'Disable' : 'Enable'} Hotel Search
+            </Button>
+
+            {isLoadingFlight ? (
                 <IconLoader className="size-4 animate-spin" />
             ) : (
                 <pre>
-                    {data
-                        ? JSON.stringify(JSON.parse(data), null, 2)
+                    {flights
+                        ? JSON.stringify(JSON.parse(flights), null, 2)
+                        : 'No data...'}
+                </pre>
+            )}
+
+            {isLoadingHotel ? (
+                <IconLoader className="size-4 animate-spin" />
+            ) : (
+                <pre>
+                    {hotels
+                        ? JSON.stringify(JSON.parse(hotels), null, 2)
                         : 'No data...'}
                 </pre>
             )}
